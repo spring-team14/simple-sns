d4f378dd1f04773ea851008a6523c214fd06b3c7
yrchoi <daylikezero@gmail.com>
2025-02-14 15:27:53 +0900

    first commit

---
+++ src/test/java/com/example/simplesns/SimpleSnsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplesns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSnsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
