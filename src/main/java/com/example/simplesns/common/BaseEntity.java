fc839db0274935c62095e494efee46393a8048db
Park-EJ <sophia5091191@gmail.com>
2025-02-14 20:00:10 +0900

    feat(commom):BaseEntity 클래스 생성

---
+++ src/main/java/com/example/simplesns/common/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.example.simplesns.common;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedAt;
+}
