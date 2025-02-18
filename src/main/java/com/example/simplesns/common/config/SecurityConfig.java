2af3897e770bf27a3229a9503493e85dcabe1eb1
yrchoi <daylikezero@gmail.com>
2025-02-18 10:20:23 +0900

    fix(total) 오류 수정

--- src/main/java/com/example/simplesns/common/config/SecurityConfig.java
+++ src/main/java/com/example/simplesns/common/config/SecurityConfig.java
@@ -30,11 +30,6 @@ public class SecurityConfig {
 
         return http.build();
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 }
 
 
