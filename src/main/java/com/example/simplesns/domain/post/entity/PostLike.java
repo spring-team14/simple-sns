886272a96f46791d01279f2e846716f810d6e91f
hanna <hannah.k0925@gmail.com>
2025-02-18 20:43:01 +0900

    feat(auth): 게시물 좋아요(토글) 기능 구현
    - 게시물에 좋아요 추가 및 취소(토글) 기능 구현
    - 사용자가 같은 게시물에 여러 번 좋아요 누르지 않도록 처리

---
+++ src/main/java/com/example/simplesns/domain/post/entity/PostLike.java
@@ -0,0 +1,30 @@
+package com.example.simplesns.domain.post.entity;
+
+import com.example.simplesns.common.entity.BaseEntity;
+import com.example.simplesns.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "post_likes")
+public class PostLike extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    public PostLike(User user, Post post) {
+        this.user = user;
+        this.post = post;
+    }
+}
---
+++ src/main/java/com/example/simplesns/domain/post/repository/PostLikesRepository.java
@@ -0,0 +1,10 @@
+package com.example.simplesns.domain.post.repository;
+
+import com.example.simplesns.domain.post.entity.PostLike;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface PostLikesRepository extends JpaRepository<PostLike, Long> {
+    Optional<PostLike> findByPostIdAndUserId(Long postId, Long userId);
+}
