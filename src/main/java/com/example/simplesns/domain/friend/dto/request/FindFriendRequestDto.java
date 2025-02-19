f581eeaf9233efb5d79f38fdae06b3fc17793222
yrchoi <daylikezero@gmail.com>
2025-02-19 20:42:10 +0900

    fix(friend): 요청 데이터 validation 적용

--- src/main/java/com/example/simplesns/domain/friend/dto/request/FindFriendRequestDto.java
+++ src/main/java/com/example/simplesns/domain/friend/dto/request/FindFriendRequestDto.java
@@ -3,7 +3,7 @@ package com.example.simplesns.domain.friend.dto.request;
 import lombok.Getter;
 
 @Getter
-public class FriendRequestDto {
+public class FindFriendRequestDto {
     private String email;
     private String name;
 }
--- src/main/java/com/example/simplesns/domain/friend/dto/request/SignOffFriendRequestDto.java
+++ src/main/java/com/example/simplesns/domain/friend/dto/request/SignOffFriendRequestDto.java
@@ -1,8 +1,10 @@
 package com.example.simplesns.domain.friend.dto.request;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 
 @Getter
 public class SignOffFriendRequestDto {
+    @NotNull(message = "승인 여부는 필수값입니다.")
     private Integer status;
 }
